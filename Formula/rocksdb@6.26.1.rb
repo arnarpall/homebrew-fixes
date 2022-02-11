f269e5116ac77920b44bbed4d76693b30f3e6824
Arnar Páll <arnar@netapp.com>
2022-02-11 11:55:49 +0000

    fixup! Create arnarpall/fixes tap

--- Formula/rocksdb@6.26.1.rb
+++ Formula/rocksdb@6.26.1.rb
@@ -1,4 +1,4 @@
-class Rocksdb < Formula
+class RocksdbAT6261 < Formula
   desc "Embeddable, persistent key-value store for fast storage"
   homepage "https://rocksdb.org/"
   url "https://github.com/facebook/rocksdb/archive/v6.26.1.tar.gz"
